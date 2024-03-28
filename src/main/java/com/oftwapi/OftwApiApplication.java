d1ec2cb15c6aa2d20dbaa4c1e6af1b17c670d7dc
Won Beom Heo <wonbeom.heo@classact.co.kr>
2024-03-28 11:13:03 +0900

    Project Initialization

---
+++ src/main/java/com/oftwapi/OftwApiApplication.java
@@ -0,0 +1,13 @@
+package com.oftwapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OftwApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OftwApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oftwapi/OftwApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oftwapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OftwApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
